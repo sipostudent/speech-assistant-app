7384f2a7ac052524ca8f142280c490e71f8fbd58
Sipo Charles <sipocharles@yahoo.co.uk>
2019-12-23 20:02:39 +0000

    implemented location search

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from time import ctime
 r = sr.Recognizer()
 
 
-def record_audio(ask=False):
+def record_audio(ask = False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+          print(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
@@ -26,10 +26,15 @@ def respond(voice_data):
     if 'what time is it' in voice_data:
         print(ctime())
     if 'search' in voice_data:
-        search = record_audio('What do you want to search for?')
-        url = 'https://google.com/search?q=' + search
-        webbrowser.get().open(url)
-        print('Here is what I found for ' + search)
+      search = record_audio('What do you want to search for?')
+      url = 'https://google.com/search?q=' + search
+      webbrowser.get().open(url)
+      print('Here is what I found for ' + search)
+    if 'location' in voice_data:
+      location = record_audio('What is the location?')
+      url = 'https://google.nl/maps/place/' + location + '/&amp;'
+      webbrowser.get().open(url)
+      print('Here is the location of ' + location)
 
 
 print('How can I help you?')
